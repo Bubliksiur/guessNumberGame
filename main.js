0eae59607dc6209f74d2751c061ea8dd2f46b64b
Bubliks <149240768+Bubliksiur@users.noreply.github.com>
2025-05-16 22:38:23 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -5,8 +5,16 @@ const warning = document.querySelector('#warning');
 
 let randomNumber = Math.floor(Math.random(1) * 100);
 
-guessButton.addEventListener('click', () => {
-    if (numberInput.value > randomNumber) warning.textContent = `Your number is too big`;
-    else if (numberInput.value < randomNumber) warning.textContent = `Your number is too low`;
-    else warning.textContent = `Nice! You win.`;
-})
+function main() {
+    if (numberInput.value > randomNumber) {
+        warning.textContent = `Your number is too big`;
+    }
+    else if (numberInput.value < randomNumber) {
+        warning.textContent = `Your number is too low`;
+    }
+    else {
+        warning.textContent = `Nice! You win.`;
+    }
+}
+
+guessButton.addEventListener('click', main)
